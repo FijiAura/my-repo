03a5b7863acc851aa822589cddb8271642153d63
FijiAura <sebastiancatania@icloud.com>
2025-03-09 21:18:57 +0800

    Update skibidi.asm

--- skibidi.asm
+++ skibidi.asm
@@ -1,1 +1,12 @@
-*skibidi toilet skibidi bop bop bop yes yes
+ *skibidi toilet
+
+BITS	32
+global	_main
+extern	_printf
+section	.text
+	push	msg
+	call	_printf
+	add		esp,4
+	ret
+section	.data
+	msg:	db	"skibidi",0Ah,0
